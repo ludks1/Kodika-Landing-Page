14842f8e8c229f18fee1233758bde142245cb71c
ludks1 <ludwindrts@gmail.com>
2025-06-09 21:04:35 -0600

    feat(projects): creating the base for projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/navbar";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <div className="bg-[#0f172a]">
         <NavBar />
         <Hero />
+        <Projects />
       </div>
     </>
   );
---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+export default function Projects() {
+  return (
+    <section className="min-h-screen flex flex-col items-center justify-center text-center px-6 bg-gray-900">
+      <div className="bg-gray-800/100 text-white p-4">
+        <h2 className="text-2xl font-bold">Nuestros Proyectos</h2>
+        <p className="mt-2">
+          Aquí hay algunos de nuestros proyectos recientes:
+        </p>
+      </div>
+    </section>
+  );
+}
