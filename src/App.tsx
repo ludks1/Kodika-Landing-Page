557355466bd2a7d70b3ed7bbb94c1c7c47be12d9
ludks1 <ludwindrts@gmail.com>
2025-06-20 13:08:34 -0600

    refactor: :art: NavBar import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Hero from "./components/Hero";
-import NavBar from "./components/navbar";
+import NavBar from "./components/Navbar";
 import Projects from "./components/Projects";
 
 export default function App() {
